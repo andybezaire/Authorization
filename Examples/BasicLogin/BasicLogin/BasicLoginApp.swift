e937f4e0afd0f2abe94ae1cb6e32a3e9746f39c7
andy <andy.bezaire@gmail.com>
2021-03-20 11:55:20 +0200

    added initial example app

---
+++ Examples/BasicLogin/BasicLogin/BasicLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLoginApp.swift
+//  BasicLogin
+//
+//  Created by Andy Bezaire on 20.3.2021.
+//
+
+import SwiftUI
+
+@main
+struct BasicLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
