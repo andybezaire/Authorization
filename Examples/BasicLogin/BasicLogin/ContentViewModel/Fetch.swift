d056403b6d04db53163cc5b7247b1a9c88c6428e
andy <andy.bezaire@gmail.com>
2021-03-21 11:30:28 +0200

    added switch functionality for expired or network errors

--- Examples/BasicLogin/BasicLogin/ContentViewModel/Fetch.swift
+++ Examples/BasicLogin/BasicLogin/ContentViewModel/Fetch.swift
@@ -14,19 +14,29 @@ extension ContentView.Model {
         let url = URL(string: "http://example.com")!
         let request = URLRequest(url: url)
 
-        Mock(sequentialMocks: [
-            Mock(url: url, dataType: .json, statusCode: 403, data: [.get: Data()]),
-            Mock(url: url, dataType: .json, statusCode: 200, data: [.get: Data()]),
-        ])?.register()
+        if isTokenExpired {
+            Mock(sequentialMocks: [
+                Mock(url: url, dataType: .json, statusCode: 403, data: [.get: Data()]),
+                Mock(url: url, dataType: .json, statusCode: 200, data: [.get: Data()]),
+            ])?.register()
+        } else {
+            Mock(url: url, dataType: .json, statusCode: 200, data: [.get: Data()]).register()
+        }
+
+        if isNetworkFailures {
+            Mock(url: url, dataType: .json, statusCode: 999, data: [.get: Data()], requestError: URLError(.badURL)).register()
+        }
 
         fetching = auth.fetch(request)
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { [unowned self] completion in
                 switch completion {
-                case .failure:
+                case .failure(let fetchError):
                     fetchStatus = "Fetch failed!"
+                    error = fetchError.localizedDescription
                 case .finished:
                     fetchStatus = "Fetch successful."
+                    error = nil
                 }
             }, receiveValue: { [unowned self] _ in
                 fetchStatus = "Fetch successful"
